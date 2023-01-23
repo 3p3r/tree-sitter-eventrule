b242c8d5722f5494e9f1ca4eae77c1fd6282eb4e
Michael Neil <mneil@mneilsworld.com>
2023-01-23 08:33:00 -0800

    Add rome and format scripts to help enforce code styles

--- webpack.config.js
+++ webpack.config.js
@@ -4,58 +4,61 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 /** @type {webpack.Configuration} */
 const config = {
-  mode: "production",
-  entry: "./rule2rego.ts",
-  target: "node",
-  devtool: false,
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    library: {
-      commonjs: "rule2rego",
-      amd: "rule2rego",
-      root: "RULE2REGO",
-    },
-    libraryTarget: "umd",
-    umdNamedDefine: true,
-    globalObject: `(typeof self !== 'undefined' ? self : this)`,
-    filename: "main.js",
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "node_modules/web-tree-sitter/tree-sitter.wasm"),
-          to: path.resolve(__dirname, "dist/tree-sitter.wasm"),
-        },
-        {
-          from: path.resolve(__dirname, "tree-sitter-eventrule.wasm"),
-          to: path.resolve(__dirname, "dist/tree-sitter-eventrule.wasm"),
-        },
-      ],
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    nodeEnv: false,
-  },
-  node: {
-    global: false,
-    __dirname: false,
-    __filename: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
-      },
-    ],
-  },
-  externalsPresets: { node: true },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
-  },
+	mode: "production",
+	entry: "./rule2rego.ts",
+	target: "node",
+	devtool: false,
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		library: {
+			commonjs: "rule2rego",
+			amd: "rule2rego",
+			root: "RULE2REGO",
+		},
+		libraryTarget: "umd",
+		umdNamedDefine: true,
+		globalObject: `(typeof self !== 'undefined' ? self : this)`,
+		filename: "main.js",
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(
+						__dirname,
+						"node_modules/web-tree-sitter/tree-sitter.wasm",
+					),
+					to: path.resolve(__dirname, "dist/tree-sitter.wasm"),
+				},
+				{
+					from: path.resolve(__dirname, "tree-sitter-eventrule.wasm"),
+					to: path.resolve(__dirname, "dist/tree-sitter-eventrule.wasm"),
+				},
+			],
+		}),
+	],
+	optimization: {
+		minimize: true,
+		nodeEnv: false,
+	},
+	node: {
+		global: false,
+		__dirname: false,
+		__filename: false,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/i,
+				loader: "ts-loader",
+				exclude: ["/node_modules/"],
+			},
+		],
+	},
+	externalsPresets: { node: true },
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+	},
 };
 
 module.exports = config;
